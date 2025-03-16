aeb136ab09acd9722d46f3241d59e290be2aa436
gogoex <110195520+gogoex@users.noreply.github.com>
2025-03-16 18:19:57 +0900

    wip

---
+++ ffi/rust/src/ffi.rs
@@ -0,0 +1,13 @@
+use crate::common::BlsctRetVal;
+
+extern "C" {
+  pub fn free_obj(x: *mut c_void);
+
+  // Scalar
+  pub fn gen_scalar(n: u64) -> *const BlsctRetVal;
+  pub fn gen_random_scalar() -> *const BlsctRetVal;
+  pub fn scalar_to_uint64(blsct_scalar: *const BlsctScalar) -> u64;
+
+  // Point
+}
+
--- ffi/rust/src/lib.rs
+++ ffi/rust/src/lib.rs
@@ -1,6 +1,5 @@
-#![allow(non_snake_case)]
-
-pub mod blsct;
 pub mod common;
+pub mod ffi;
 pub mod scalar;
+pub mod point;
 
