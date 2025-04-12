7d3fcc3183969cc1eec2338a7c1a04d2b9c51620
gogoex <110195520+gogoex@users.noreply.github.com>
2025-04-12 09:10:19 +0900

    copy secp256k1 lib

--- script/deploy-libblsct.sh
+++ script/deploy-libblsct.sh
@@ -6,4 +6,4 @@ cp $repo_dir/src/libblsct.a ./lib
 cp $repo_dir/src/libunivalue_blsct.a ./lib
 cp $repo_dir/src/bls/lib/libbls384_256.a ./lib
 cp $repo_dir/src/bls/mcl/lib/libmcl.a ./lib
-
+cp $repo_dir/src/secp256k1/.libs/libsecp256k1.a ./lib
